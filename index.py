9e232b70dc7b625814f90a881fff444e70f2baad
Keerttna <127674493+Keerttna@users.noreply.github.com>
2025-04-06 15:34:10 +0530

    Completed layout

--- index.py
+++ index.py
@@ -1,1 +1,20 @@
 # python-3.10.9
+
+import dash_bootstrap_components as dbc
+from dash import Dash
+from layout import AppLayout
+from app_callback import AppCallback
+
+styles = [dbc.themes.BOOTSTRAP]
+app = Dash(name = __name__, external_stylesheets=styles)
+app.title = "AI Content Detector"
+app.config["suppress_callback_exceptions"] = True
+server = app.server
+
+layout = AppLayout()
+app.layout = layout.getAppLayout()
+AppCallback(app)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=False)
